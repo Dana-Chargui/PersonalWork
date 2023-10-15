b5d2e259a5eed73c84f974fe6a1cd21e79660f22
Dana-Chargui <130105662+Dana-Chargui@users.noreply.github.com>
2023-10-16 00:32:40 +0400

    Create Hypotenuse.java

---
+++ Hypotenuse.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Hypo {
+    public static void main(String[] args) {
+        double x;
+        double y;
+        double z;
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter side x : ");
+        x = scan.nextDouble();
+        System.out.println("Enter side y : ");
+        y = scan.nextDouble();
+        
+        z = Math.sqrt((x*x)+(y*y));
+        
+        System.out.println("The Hypotenuse is : "+z);
+        scan.close();
+
+    }
+}
